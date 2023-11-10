25dd8d80e1ed823a8151040d772bde5a3dd848e6
Lucas Taling <lutaling@live.co.uk>
2023-11-10 13:41:31 +0000

    Improved docstring formatting

--- inflammation/models.py
+++ inflammation/models.py
@@ -32,7 +32,6 @@ def load_json(filename):
     ]
 
     :param filename: Filename of CSV to load
-
     """
     with open(filename, 'r', encoding='utf-8') as file:
         data_as_json = json.load(file)
@@ -41,16 +40,31 @@ def load_json(filename):
 
 
 def daily_mean(data):
-    """Calculate the daily mean of a 2D inflammation data array."""
+    """Calculate the daily mean of a 2D inflammation data array.
+
+       :param data: Data of which to calculate the mean
+
+       :returns: the mean of the data as a 1D numpy array
+    """
     return np.mean(data, axis=0)
 
 
 def daily_max(data):
-    """Calculate the daily max of a 2D inflammation data array."""
+    """Calculate the daily max of a 2D inflammation data array.
+    
+       :param data: Data of which to calculate the maximum 
+
+       :returns: the maximum value of the data as a 1D numpy array
+    """
     return np.max(data, axis=0)
 
 
 def daily_min(data):
-    """Calculate the daily min of a 2D inflammation data array."""
+    """Calculate the daily min of a 2D inflammation data array.
+    
+       :param data: Data of which to calculate the minimum
+
+       :returns: the minimum value of the data as a 1D numpy array
+    """
     return np.min(data, axis=0)
 
