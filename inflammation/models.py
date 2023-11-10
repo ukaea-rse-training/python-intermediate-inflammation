b6ff9cd000e4f38e9e4dbe4e841f4281283055c2
jbgitusr <bennet.jose@ukaea.uk>
2023-11-10 13:59:32 +0000

    Pylint improvement

--- inflammation/models.py
+++ inflammation/models.py
@@ -43,7 +43,8 @@ def load_json(filename):
 def daily_mean(data):
     """Calculate the daily mean of a 2D inflammation data array.
     
-    :param data: A 2D data array with inflammation data (each row contains measurements for a single patient across all days).
+    :param data: A 2D data array with inflammation data (each row 
+        contains measurements for a single patient across all days).
     :returns: An array of mean values of measurements for each day.
     """
     return np.mean(data, axis=0)
@@ -52,7 +53,8 @@ def daily_mean(data):
 def daily_max(data):
     """Calculate the daily max of a 2D inflammation data array.
 
-    :param data: A 2D data array with inflammation data (each row contains measurements for a single patient across all days).
+    :param data: A 2D data array with inflammation data (each row 
+        contains measurements for a single patient across all days).
     :returns: An array of maximum values of measurements for each day.
     """
     return np.max(data, axis=0)
@@ -61,8 +63,8 @@ def daily_max(data):
 def daily_min(data):
     """Calculate the daily min of a 2D inflammation data array.
 
-    :param data: A 2D data array with inflammation data (each row contains measurements for a single patient across all days).
+    :param data: A 2D data array with inflammation data (each row 
+        contains measurements for a single patient across all days).
     :returns: An array of minimum values of measurements for each day.
     """
     return np.min(data, axis=0)
-
