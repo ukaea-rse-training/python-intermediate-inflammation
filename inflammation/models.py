0c2c8b1fdcf1cdb3491d7068b9f1b1f9418a5706
ltaling <99805596+ltaling@users.noreply.github.com>
2023-11-23 16:03:31 +0000

    Implement suggested changes

--- inflammation/models.py
+++ inflammation/models.py
@@ -55,12 +55,12 @@ def daily_min(data):
     return np.min(data, axis=0)
 
 
-def s_dev(data):
-    """Computes and returns standard deviation for data."""
-    mmm = np.mean(data, axis=0)
+def standard_deviation(data):
+    """Calculate the standard deviation of a 2d inflammation data array and return it as a dictionary."""
+    mean_data = daily_mean(data)
     devs = []
     for entry in data:
-        devs.append((entry - mmm) * (entry - mmm))
+        devs.append((entry - mean_data) * (entry - mean_data))
 
     s_dev2 = sum(devs) / len(data)
     return {'standard deviation': s_dev2}
